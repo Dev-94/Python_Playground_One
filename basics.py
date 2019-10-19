fc35cd96023f6e0454f93b77564f13408347ffcf
Dev Govindji <friendsofdev94@gmail.com>
2019-10-19 16:26:28 -0500

    adds list, range, and range with step for play

--- basics.py
+++ basics.py
@@ -13,9 +13,18 @@ x = 10
 y = "10"
 z = 10.1
 
+
 sum1 = x + x
 sum2 = y + y
 
 print(sum1, sum2)
-
 print(type(x), type(y), type(z))
+
+
+student_grades = [9.1, 9.8, 7.5]
+grade_range = list(range(1, 10))
+range_step = list(range(1, 10, 3))
+
+print(student_grades)
+print(grade_range)
+print(range_step)
